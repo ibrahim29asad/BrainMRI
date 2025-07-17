fa6e79da61be83c0441e99bdb29769ac7702c90e
ibrahim29asad <ibrahim.asad2019@gmail.com>
2025-07-16 19:45:16 -0600

    Importing SpringBoot

---
+++ BrainMRI_Backend/src/main/java/com/example/BrainMRI_Backend/BrainMriBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.BrainMRI_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainMriBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainMriBackendApplication.class, args);
+	}
+
+}
---
+++ BrainMRI_Backend/src/test/java/com/example/BrainMRI_Backend/BrainMriBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BrainMRI_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainMriBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
